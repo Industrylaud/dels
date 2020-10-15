a08350846e052e83b1359af2613dec07f5c1a572
Industrylaud <mrfros22@gmail.com>
2020-10-15 21:16:22 +0200

    basic tests for pages

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,15 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomePageTest(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def testHomePageStatusCode(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def testHomePageTemplate(self):
+        self.assertTemplateUsed(self.response, 'home.html')
