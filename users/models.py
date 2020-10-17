08d935f9e3510c29f3dc719a34c946f1984d8a2f
Industrylaud <mrfros22@gmail.com>
2020-10-17 21:48:55 +0200

    add group model to users and updated user fields

--- users/models.py
+++ users/models.py
@@ -1,6 +1,29 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
+
+
+class StudentGroup(models.Model):
+    name = models.CharField(unique=True, null=False, blank=False, max_length=255)
+
+    def __str__(self):
+        return self.name
 
 
 class CustomUser(AbstractUser):
-    pass
+    index_number = models.CharField(max_length=6, unique=True, null=True, blank=True)
+    student_group = models.ForeignKey(StudentGroup, on_delete=models.SET_NULL, null=True, blank=True)
+
+    A, B, C, D, E = 1, 2, 3, 4, 5
+    SUB_GROUP = (
+        (A, _('A')),
+        (B, _('B')),
+        (C, _('C')),
+        (D, _('D')),
+        (E, _('E')),
+    )
+    sub_group = models.PositiveSmallIntegerField(choices=SUB_GROUP, default=1, null=True, blank=True)
+
+
+
+
