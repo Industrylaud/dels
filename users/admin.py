98cdcf3ca81c43a3a0a59d37b546bc41d1202fa5
Industrylaud <mrfros22@gmail.com>
2020-10-18 21:37:57 +0200

    admin site update

--- users/admin.py
+++ users/admin.py
@@ -2,16 +2,31 @@ from django.contrib import admin
 from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import StudentGroup
 
 
 CustomUser = get_user_model()
 
 
+class CustomUsersInLine(admin.TabularInline):
+    model = CustomUser
+    extra = 0
+    fields = ['index_number', 'last_name', 'first_name', 'email',]
+
+
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
-    form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model = CustomUser
     list_display = ['email', 'username', 'first_name', 'last_name', ]
 
 
+class StudentGroupAdmin(admin.ModelAdmin):
+    inlines = [
+        CustomUsersInLine,
+    ]
+    list_display = ('name',)
+
+
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(StudentGroup, StudentGroupAdmin)
