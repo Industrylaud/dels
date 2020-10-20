91f20e0a0352366df9ccef0e1a5e695467921bbf
Industrylaud <mrfros22@gmail.com>
2020-10-20 14:47:55 +0200

    environmental variables settings

--- dels_project/settings.py
+++ dels_project/settings.py
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '57qf6mryrrm#-(wmdo2)f8-2!bdq9*-s&o==s$0(3d20mz9e02'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', default=0))
 
 ALLOWED_HOSTS = []
 
