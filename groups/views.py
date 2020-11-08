38a11cedd891f52c7e928d074f265cf208a1ac7a
industrylaud <mrfros22@gmail.com>
2020-11-08 16:13:13 +0100

    add extra content to view

--- groups/views.py
+++ groups/views.py
@@ -1,9 +1,15 @@
 from django.views.generic import ListView
+from django.views.generic.edit import CreateView
 
 from groups.models import Post
 from users.models import StudentGroup
 
 
-class GroupView(ListView):
+class GroupView(CreateView):
     model = Post
     template_name = 'groups/student_group.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['posts'] = self.model.objects.all()
+        return context
