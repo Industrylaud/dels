d0a64d5d2ff0bcd1eb936f991f31332d52dc02b6
industrylaud <mrfros22@gmail.com>
2020-12-12 17:19:22 +0100

    add path to post detail for teacher

--- subjects/urls.py
+++ subjects/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('<int:pk>/teacher', TeacherSubjectView.as_view(), name='teacher_subject'),
     path('<int:pk>/teacher/post_create', TeacherPostCreationView.as_view(), name='post_create_teacher'),
     path('<int:pk>/teacher/task_create', TeacherTaskCreationView.as_view(), name='task_create_teacher'),
+    path('<int:pk>/teacher/post_detail/<uuid:pk>', TeacherTaskCreationView.as_view(), name='post_detail_teacher'),
 ]
