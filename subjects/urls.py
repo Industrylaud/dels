0eb2b236aabea1bc1c85cd802e00972bff214d45
industrylaud <mrfros22@gmail.com>
2020-12-12 18:41:41 +0100

    add task detail view

--- subjects/urls.py
+++ subjects/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from subjects.views import TeacherSubjectView, TeacherPostCreationView, TeacherTaskCreationView, SubjectPostDetailView
+from subjects.views import *
 
 urlpatterns = [
     path('<int:pk>/teacher', TeacherSubjectView.as_view(), name='teacher_subject'),
     path('<int:pk>/teacher/post_create', TeacherPostCreationView.as_view(), name='post_create_teacher'),
     path('<int:pk>/teacher/task_create', TeacherTaskCreationView.as_view(), name='task_create_teacher'),
     path('<int:pk>/post_detail/<int:id>', SubjectPostDetailView.as_view(), name='post_detail_subject'),
+    path('<int:pk>/task_detail/<int:id>', TaskDetailView.as_view(), name='task_detail_subject'),
 ]
--- subjects/views.py
+++ subjects/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import get_object_or_404
 from django.views.generic import ListView, CreateView
 
 from .forms import TaskCreationForm
-from .models import PostInSubject, Task, Resource, Subject, CommentInSubject
+from .models import PostInSubject, Task, Resource, Subject, CommentInSubject, CommentTask
 
 
 class TeacherSubjectView(LoginRequiredMixin, ListView):
@@ -82,4 +82,40 @@ class TeacherTaskCreationView(LoginRequiredMixin, CreateView):
 
     def form_valid(self, form):
         form.instance.subject = Subject.objects.get(id=int(self.kwargs['pk']))
+        return super().form_valid(form)
+
+
+class TaskDetailView(LoginRequiredMixin, CreateView):
+    model = CommentTask
+    template_name = 'subjects/task_detail.html'
+    fields = [
+        'body',
+    ]
+
+    def get(self, request, *args, **kwargs):
+        subject = get_object_or_404(Subject, pk=int(self.kwargs['pk']))
+
+        if get_object_or_404(subject.teachers, teacher=self.request.user) or \
+                get_object_or_404(subject.students, user=self.request.user):
+            return super().get(request, *args, **kwargs)
+
+        return HttpResponseForbidden()
+
+    def get_context_data(self, **kwargs):
+        task = get_object_or_404(Task, pk=int(self.kwargs['id']))
+        subject = get_object_or_404(Subject, pk=int(self.kwargs['pk']))
+        user = get_user_model()
+
+        if get_object_or_404(subject.teachers, teacher=self.request.user) or \
+                get_object_or_404(subject.students, user=self.request.user):
+            context = super().get_context_data(**kwargs)
+            context['task'] = task
+            context['comments'] = self.model.objects.filter(task_id=int(self.kwargs['id'])).order_by('-pub_date')
+            return context
+
+        return HttpResponseForbidden()
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        form.instance.task = Task.objects.get(id=int(self.kwargs['pk']))
         return super().form_valid(form)
